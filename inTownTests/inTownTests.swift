0833b927a0ff0a2de1c74c97f6c96d6357636afc
yuya <nerentabelen@mail.ru>
2025-10-03 18:32:29 +0300

    initial

---
+++ inTownTests/inTownTests.swift
@@ -0,0 +1,17 @@
+//
+//  inTownTests.swift
+//  inTownTests
+//
+//  Created by Влад Рыжкин on 03.10.2025.
+//
+
+import Testing
+@testable import inTown
+
+struct inTownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
